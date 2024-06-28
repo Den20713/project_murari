330d77d321958a12527810fae6e1dcf01ed6108a
Den20713 <denusklijuk19@gmail.com>
2024-06-28 19:02:12 +0300

    фвв

--- script.js
+++ script.js
@@ -114,3 +114,13 @@ document.addEventListener('DOMContentLoaded', function() {
   // Оновлення загальної суми та загальної суми на сторінці кошика при завантаженні сторінки
   updateTotalPriceOnCart();
 });
+
+
+let block = document.querySelector('.mn_wear_product')
+document.addEventListener('mousemove', function(e) {
+    let dx = e.pageX - window.innerWidth / 2
+    let dy = e.pageY - window.innerHeight / 2
+    let angleX = 20 * dx / window.innerWidth / 2
+    let angleY = 20 * dy / window.innerHeight / 2
+    block.style.transform = `rotateX(${-angleY}deg) rotateY(${angleX}deg)`
+})
